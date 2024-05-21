26cb1248c9f2cc978ec32cece4db8e8a390553cb
ives <ivaldo.raul@hotmail.com>
2024-05-21 16:29:48 -0300

    build: commit inicial

---
+++ src/main/java/com/example/springdois/SpringdoisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdois/SpringdoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
